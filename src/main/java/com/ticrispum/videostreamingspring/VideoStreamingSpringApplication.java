a23becbbc53a5feca349e90b1e90189e060c33cf
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2021-12-30 23:23:30 +0100

    Create a stub Spring Web Flux project

---
+++ src/main/java/com/ticrispum/videostreamingspring/VideoStreamingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ticrispum.videostreamingspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamingSpringApplication.class, args);
+	}
+
+}
